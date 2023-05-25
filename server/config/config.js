51413451f1874f7a958c2962c978b14491487e6c
josefucoder <josefucoder@gmail.com>
2023-05-25 12:12:40 -0700

    Update RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -13,10 +13,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-// if (process.env.NODE_ENV === 'dev' ) {
-//     urlDB = 'mongodb://127.0.0.1:27017/cafe';
-// } else {
+if (process.env.NODE_ENV === 'dev' ) {
+    urlDB = 'mongodb://127.0.0.1:27017/cafe';
+} else {
    urlDB = "mongodb://mongo:lPJvplHlGQLCuTPY9etn@containers-us-west-75.railway.app:5512"
-// }
+}
 
 process.env.URLDB = urlDB;
