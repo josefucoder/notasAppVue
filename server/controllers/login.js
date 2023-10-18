4552785a75533644776497fa05436a9d509f244e
josefucoder <josefucoder@gmail.com>
2023-10-18 10:55:06 -0700

    Ajustes finales

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -4,51 +4,6 @@ const jwt = require('jsonwebtoken');
 
 const Usuario = require("../models/usuario");
 
-const login = async( req, res )=> {
-    
-    let body = req.body;
-
-    Usuario.findOne({ email: body.email }).then((user)=>{
-
-         if(!user) {
-            return res.status(400).json({
-                ok: false,
-                err: {
-                    message: '(Usuario) o Contraseña incorrectos.'
-                }
-              });
-         }
-
-         if(!bcrypt.compareSync( body.password, user.password) ) {
-            return res.status(400).json({
-                ok: false,
-                err: {
-                    message: 'Usuario o (Contraseña) incorrectos.'
-                }
-              });
-         }
-
-         let token = jwt.sign({
-            usuario: user
-         }, process.env.SEED , { expiresIn: process.env.CADUCIDAD_TOKEN });
-
-         res.json({
-            ok: true,
-            usuario: user,
-            token
-         });
-
-    })
-    .catch(err=> {
-        
-        return res.status(500).json({
-            ok: false,
-            err
-          });
-    })
-
-};
-
 const loginForm = async( req, res )=> {
     
     let body = req.body;
@@ -96,7 +51,6 @@ const loginForm = async( req, res )=> {
 
 module.exports = {
 
-    login, 
     loginForm
     
 };
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
-const { login, loginForm } = require('../controllers/login');
-
-app.post('/login', login );
+const { loginForm } = require('../controllers/login');
 
 app.post('/loginForm', loginForm );
 
